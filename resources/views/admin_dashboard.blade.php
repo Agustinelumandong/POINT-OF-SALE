e522d5a59a90174c8c314d6f808aa2a4d6aaba57
Sean Agustine Esparagoza <agustinelumandong@gmail.com>
2024-12-01 22:29:27 +0800

    Font Awsome cdn added

--- resources/views/admin_dashboard.blade.php
+++ resources/views/admin_dashboard.blade.php
@@ -42,8 +42,8 @@
   <!-- SweetAlert2 CSS -->
   <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/sweetalert2@11/dist/sweetalert2.min.css">
 
-
-
+  <!-- font-awesome -->
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.7.1/css/all.min.css" integrity="sha512-5Hs3dF2AEPkpNAR7UiOHba+lRSJNeM2ECkwxUIxC1Q/FLycGTbNapWXB4tP889k5T5Ju8fs4b1P5z/iB4nMfSQ==" crossorigin="anonymous" referrerpolicy="no-referrer" />
 
 </head>
 
